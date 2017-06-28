f1941bd93aa1f864b616237bb8b6439ff175484e
Jonathan Romualdo <jrsap@a.recife.ifpe.edu.br>
2017-06-28 14:46:28 -0300

    banco

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,21 +75,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'postgresql': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_database',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
     }
-
-
 }
 
 
