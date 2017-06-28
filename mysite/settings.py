de0752e217830942cea89d691e677b49a46c5f84
Jonathan Romualdo <jrsap@a.recife.ifpe.edu.br>
2017-06-28 14:41:38 -0300

    Trocando banco de dados.

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,7 +75,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'postgresql': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_database',
         'USER': 'postgres',
@@ -83,6 +88,8 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
+
+
 }
 
 
