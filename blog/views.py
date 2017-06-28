6f59885bc681cb6f881069b35624101d734b8e49
Jonathan Romualdo <jrsap@a.recife.ifpe.edu.br>
2017-06-28 16:27:32 -0300

    Visualização de publicações.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
+# Como views.py e models.py estão no mesmo diretório podemos simplesmente usar . e o nome do arquivo (sem .py).
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+
+    #Pegar a lista de posts que foram publicados
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+    #Retornar os posts publicados (passando o resultado para o template)
+    return render(request, 'blog/post_list.html', {'posts': posts})
