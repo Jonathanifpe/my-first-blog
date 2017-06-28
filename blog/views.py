c03c571207a61924888d27f8a48d7a1d0b660879
Jonathan Romualdo <jrsap@a.recife.ifpe.edu.br>
2017-06-28 18:59:44 -0300

    CSS, link, finalizado

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 # Como views.py e models.py estão no mesmo diretório podemos simplesmente usar . e o nome do arquivo (sem .py).
 from .models import Post
@@ -6,9 +6,13 @@ from .models import Post
 # Create your views here.
 
 def post_list(request):
-
     #Pegar a lista de posts que foram publicados
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
     #Retornar os posts publicados (passando o resultado para o template)
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail (request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
